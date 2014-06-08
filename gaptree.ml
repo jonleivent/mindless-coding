c7433c4c872c9cd9141489cd4ed5edb02a6e178d
jonleivent <jonleivent@users.noreply.github.com>
2014-06-08 16:53:20 -0400

    extracted OCaml gaptree code

---
+++ gaptree.ml
@@ -0,0 +1,419 @@
+type 'a option =
+| Some of 'a
+| None
+
+type comparison =
+| Eq
+| Lt
+| Gt
+
+type compareSpecT =
+| CompEqT
+| CompLtT
+| CompGtT
+
+type 'a sig0 =
+  'a
+  (* singleton inductive, whose constructor was exist *)
+
+type sumbool =
+| Left
+| Right
+
+type 'a eqDec = 'a -> 'a -> sumbool
+
+type 'a ordered = { eq_dec : 'a eqDec; compare : ('a -> 'a -> comparison); compare_spec : ('a -> 'a -> compareSpecT) }
+
+(** val compare_spec : 'a1 ordered -> 'a1 -> 'a1 -> compareSpecT **)
+
+let compare_spec x = x.compare_spec
+
+type a (* AXIOM TO BE REALIZED *)
+
+(** val ordA : a ordered **)
+
+let ordA =
+  failwith "AXIOM TO BE REALIZED"
+
+type gap =
+| G1
+| G0
+
+type gaptree =
+| Leaf
+| Node of gap * gaptree * a * gaptree
+
+type findResult =
+| Found
+| NotFound
+
+(** val find : a -> gaptree -> findResult **)
+
+let rec find x = function
+| Leaf -> NotFound
+| Node (g, tl, d, tr) ->
+  (match ordA.compare_spec x d with
+   | CompEqT -> Found
+   | CompLtT -> find x tl
+   | CompGtT -> find x tr)
+
+(** val gof : gaptree -> gap option **)
+
+let gof = function
+| Leaf -> None
+| Node (g, t1, d, t2) -> Some g
+
+(** val setGap : gap -> gaptree -> gaptree **)
+
+let setGap ng = function
+| Leaf -> Leaf
+| Node (g, t1, d, t2) -> Node (ng, t1, d, t2)
+
+(** val regapAs : gaptree -> gaptree -> gaptree **)
+
+let regapAs t ast =
+  match gof ast with
+  | Some g0 -> setGap g0 t
+  | None -> Leaf
+
+type regapR =
+  gaptree
+  (* singleton inductive, whose constructor was regapR *)
+
+(** val regapAs' : gaptree -> gaptree -> regapR **)
+
+let regapAs' t ast =
+  match gof ast with
+  | Some g0 -> setGap g0 t
+  | None -> assert false (* absurd case *)
+
+type gapnode =
+  gaptree
+  (* singleton inductive, whose constructor was Gapnode *)
+
+type ires =
+| ISameH
+| Higher
+
+type insertResult =
+| FoundByInsert
+| Inserted of gaptree * ires
+
+(** val rotateRight : gaptree -> a -> gaptree -> gap -> gapnode **)
+
+let rotateRight tl d tr go =
+  match tl with
+  | Leaf -> assert false (* absurd case *)
+  | Node (g0, tl0, d0, tr0) ->
+    (match tr0 with
+     | Leaf -> Node (go, tl0, d0, (Node (G0, Leaf, d, Leaf)))
+     | Node (g1, g0_1, d1, g0_2) ->
+       (match g1 with
+        | G1 -> Node (go, tl0, d0, (Node (G0, (setGap G0 tr0), d, (setGap G0 tr))))
+        | G0 -> Node (go, (Node (G0, (setGap G0 tl0), d0, g0_1)), d1, (Node (G0, g0_2, d, (setGap G0 tr))))))
+
+(** val rotateLeft : gaptree -> a -> gaptree -> gap -> gapnode **)
+
+let rotateLeft tl d tr go =
+  match tr with
+  | Leaf -> assert false (* absurd case *)
+  | Node (g0, tl0, d0, tr0) ->
+    (match tl0 with
+     | Leaf -> Node (go, (Node (G0, Leaf, d, Leaf)), d0, tr0)
+     | Node (g1, g0_1, d1, g0_2) ->
+       (match g1 with
+        | G1 -> Node (go, (Node (G0, (setGap G0 tl), d, (setGap G0 tl0))), d0, tr0)
+        | G0 -> Node (go, (Node (G0, (setGap G0 tl), d, g0_1)), d1, (Node (G0, g0_2, d0, (setGap G0 tr0))))))
+
+(** val insert : a -> gaptree -> insertResult **)
+
+let rec insert x = function
+| Leaf -> Inserted ((Node (G0, Leaf, x, Leaf)), Higher)
+| Node (g, tl, d, tr) ->
+  (match ordA.compare_spec x d with
+   | CompEqT -> FoundByInsert
+   | CompLtT ->
+     (match insert x tl with
+      | FoundByInsert -> FoundByInsert
+      | Inserted (t0, i) ->
+        (match i with
+         | ISameH -> Inserted ((Node (g, t0, d, tr)), ISameH)
+         | Higher ->
+           (match gof tl with
+            | Some g0 ->
+              (match g0 with
+               | G1 -> Inserted ((Node (g, t0, d, tr)), ISameH)
+               | G0 ->
+                 (match gof tr with
+                  | Some g1 ->
+                    (match g1 with
+                     | G1 -> Inserted ((rotateRight t0 d tr g), ISameH)
+                     | G0 -> Inserted ((Node (G0, t0, d, (setGap G1 tr))), Higher))
+                  | None -> Inserted ((rotateRight t0 d tr g), ISameH)))
+            | None ->
+              (match gof tr with
+               | Some g0 -> Inserted ((Node (g, t0, d, tr)), ISameH)
+               | None -> Inserted ((Node (G0, t0, d, Leaf)), Higher)))))
+   | CompGtT ->
+     (match insert x tr with
+      | FoundByInsert -> FoundByInsert
+      | Inserted (t0, i) ->
+        (match i with
+         | ISameH -> Inserted ((Node (g, tl, d, t0)), ISameH)
+         | Higher ->
+           (match gof tr with
+            | Some g0 ->
+              (match g0 with
+               | G1 -> Inserted ((Node (g, tl, d, t0)), ISameH)
+               | G0 ->
+                 (match gof tl with
+                  | Some g1 ->
+                    (match g1 with
+                     | G1 -> Inserted ((rotateLeft tl d t0 g), ISameH)
+                     | G0 -> Inserted ((Node (G0, (setGap G1 tl), d, t0)), Higher))
+                  | None -> Inserted ((rotateLeft tl d t0 g), ISameH)))
+            | None ->
+              (match gof tl with
+               | Some g0 -> Inserted ((Node (g, tl, d, t0)), ISameH)
+               | None -> Inserted ((Node (G0, Leaf, d, t0)), Higher))))))
+
+type dres =
+| DSameH
+| Lower
+
+type tryLowerResult =
+| Lowered of gaptree
+| LowerFailed
+
+(** val tryLowering : gaptree -> tryLowerResult **)
+
+let tryLowering = function
+| Leaf -> assert false (* absurd case *)
+| Node (g, tl, d, tr) ->
+  (match gof tl with
+   | Some g0 ->
+     (match g0 with
+      | G1 ->
+        (match gof tr with
+         | Some g1 ->
+           (match g1 with
+            | G1 -> Lowered (Node (G0, (setGap G0 tl), d, (setGap G0 tr)))
+            | G0 -> LowerFailed)
+         | None -> assert false (* absurd case *))
+      | G0 -> LowerFailed)
+   | None -> LowerFailed)
+
+(** val dRotateLeft : gaptree -> a -> gaptree -> gap -> gapnode **)
+
+let dRotateLeft tl d tr go =
+  match tr with
+  | Leaf -> assert false (* absurd case *)
+  | Node (g, tl0, d0, tr0) ->
+    (match tl0 with
+     | Leaf -> Node (go, (Node (G1, tl, d, Leaf)), d0, (setGap G1 tr0))
+     | Node (g1, g2, d1, g3) ->
+       (match g1 with
+        | G1 -> Node (go, (Node (G0, (regapAs tl tl0), d, tl0)), d0, (setGap G1 tr0))
+        | G0 ->
+          (match gof tr0 with
+           | Some g0 ->
+             (match g0 with
+              | G1 -> Node (go, (Node (G1, (setGap G0 tl), d, g2)), d1, (Node (G1, g3, d0, (setGap G0 tr0))))
+              | G0 -> Node (go, (Node (G0, (setGap G1 tl), d, tl0)), d0, (setGap G1 tr0)))
+           | None -> Node (go, (Node (G1, tl, d, g2)), d1, (Node (G1, g3, d0, tr0))))))
+
+type delminResult =
+| NoMin
+| MinDeleted of a * ( * )
+
+(** val dFitLeft : gap -> gaptree -> gaptree -> a -> gaptree -> ( * ) **)
+
+let dFitLeft g tl t' d tr =
+  match gof tr with
+  | Some g0 ->
+    (match g0 with
+     | G1 -> (Node (G1, (regapAs' t' tl), d, (setGap G0 tr))),Lower
+     | G0 ->
+       (match gof tl with
+        | Some g1 ->
+          (match g1 with
+           | G1 ->
+             let t = tryLowering tr in
+             (match t with
+              | Lowered t0 -> (Node (G1, (setGap G1 t'), d, t0)),Lower
+              | LowerFailed -> (dRotateLeft t' d tr g),DSameH)
+           | G0 -> (Node (g, t', d, tr)),DSameH)
+        | None -> assert false (* absurd case *)))
+  | None -> (Node (G1, t', d, tr)),Lower
+
+(** val delmin : gaptree -> delminResult **)
+
+let rec delmin = function
+| Leaf -> NoMin
+| Node (g0, tl, d, tr) ->
+  (match delmin tl with
+   | NoMin -> MinDeleted (d, ((setGap G1 tr),Lower))
+   | MinDeleted (m, dr) ->
+     let t,dc = dr in
+     MinDeleted (m,
+     (match dc with
+      | DSameH -> (Node (g0, t, d, tr)),DSameH
+      | Lower -> dFitLeft g0 tl t d tr)))
+
+(** val dRotateRight : gaptree -> a -> gaptree -> gap -> gapnode **)
+
+let dRotateRight tl d tr go =
+  match tl with
+  | Leaf -> assert false (* absurd case *)
+  | Node (g, tl0, d0, tr0) ->
+    (match tr0 with
+     | Leaf -> Node (go, (setGap G1 tl0), d0, (Node (G1, Leaf, d, tr)))
+     | Node (g1, g2, d1, g3) ->
+       (match g1 with
+        | G1 -> Node (go, (setGap G1 tl0), d0, (Node (G0, tr0, d, (regapAs tr tr0))))
+        | G0 ->
+          (match gof tl0 with
+           | Some g0 ->
+             (match g0 with
+              | G1 -> Node (go, (Node (G1, (setGap G0 tl0), d0, g2)), d1, (Node (G1, g3, d, (setGap G0 tr))))
+              | G0 -> Node (go, (setGap G1 tl0), d0, (Node (G0, tr0, d, (setGap G1 tr)))))
+           | None -> Node (go, (Node (G1, tl0, d0, g2)), d1, (Node (G1, g3, d, tr))))))
+
+(** val dFitRight : gap -> gaptree -> a -> gaptree -> gaptree -> ( * ) **)
+
+let dFitRight g tl d tr t' =
+  match gof tl with
+  | Some g0 ->
+    (match g0 with
+     | G1 -> (Node (G1, (setGap G0 tl), d, (regapAs' t' tr))),Lower
+     | G0 ->
+       (match gof tr with
+        | Some g1 ->
+          (match g1 with
+           | G1 ->
+             let t = tryLowering tl in
+             (match t with
+              | Lowered t0 -> (Node (G1, t0, d, (setGap G1 t'))),Lower
+              | LowerFailed -> (dRotateRight tl d t' g),DSameH)
+           | G0 -> (Node (g, tl, d, t')),DSameH)
+        | None -> assert false (* absurd case *)))
+  | None -> (Node (G1, tl, d, t')),Lower
+
+type delmaxResult =
+| NoMax
+| MaxDeleted of a * ( * )
+
+(** val delmax : gaptree -> delmaxResult **)
+
+let rec delmax = function
+| Leaf -> NoMax
+| Node (g0, tl, d, tr) ->
+  (match delmax tr with
+   | NoMax -> MaxDeleted (d, ((setGap G1 tl),Lower))
+   | MaxDeleted (m, dr) ->
+     let t,dc = dr in
+     MaxDeleted (m,
+     (match dc with
+      | DSameH -> (Node (g0, tl, d, t)),DSameH
+      | Lower -> dFitRight g0 tl d tr t)))
+
+type deleteResult =
+| DelNotFound
+| Deleted of ( * )
+
+type twoGaps =
+| NoneNone
+| G0G0
+| G1G1
+| NoneG0
+| G1G0
+| G0None
+| G0G1
+
+(** val gof2 : gaptree -> gaptree -> twoGaps **)
+
+let gof2 t1 t2 =
+  match gof t1 with
+  | Some g ->
+    (match gof t2 with
+     | Some g0 ->
+       (match g0 with
+        | G1 ->
+          (match g with
+           | G1 -> G1G1
+           | G0 -> G0G1)
+        | G0 ->
+          (match g with
+           | G1 -> G1G0
+           | G0 -> G0G0))
+     | None ->
+       (match g with
+        | G1 -> assert false (* absurd case *)
+        | G0 -> G0None))
+  | None ->
+    (match gof t2 with
+     | Some g ->
+       (match g with
+        | G1 -> assert false (* absurd case *)
+        | G0 -> NoneG0)
+     | None -> NoneNone)
+
+(** val delete : a -> gaptree -> deleteResult **)
+
+let rec delete x = function
+| Leaf -> DelNotFound
+| Node (g, tl, d, tr) ->
+  (match ordA.compare_spec x d with
+   | CompEqT ->
+     Deleted
+       (match gof2 tl tr with
+        | NoneNone -> Leaf,Lower
+        | G0G0 ->
+          let h = delmin tr in
+          (match h with
+           | NoMin -> assert false (* absurd case *)
+           | MinDeleted (m, dr) ->
+             let t0,dc = dr in
+             (match dc with
+              | DSameH -> (Node (g, tl, m, t0)),DSameH
+              | Lower -> (Node (g, (setGap G0 tl), m, t0)),DSameH))
+        | G1G1 ->
+          let h = delmin tr in
+          (match h with
+           | NoMin -> assert false (* absurd case *)
+           | MinDeleted (m, dr) ->
+             let t0,dc = dr in
+             (match dc with
+              | DSameH -> (Node (g, tl, m, t0)),DSameH
+              | Lower -> (Node (G1, (setGap G0 tl), m, t0)),Lower))
+        | NoneG0 -> (setGap G1 tr),Lower
+        | G1G0 ->
+          let h = delmin tr in
+          (match h with
+           | NoMin -> assert false (* absurd case *)
+           | MinDeleted (m, dr) -> let t0,dc = dr in (Node (g, tl, m, t0)),DSameH)
+        | G0None -> (setGap G1 tl),Lower
+        | G0G1 ->
+          let h = delmax tl in
+          (match h with
+           | NoMax -> assert false (* absurd case *)
+           | MaxDeleted (m, dr) -> let t0,dc = dr in (Node (g, t0, m, tr)),DSameH))
+   | CompLtT ->
+     (match delete x tl with
+      | DelNotFound -> DelNotFound
+      | Deleted dr ->
+        let t0,dc = dr in
+        Deleted
+        (match dc with
+         | DSameH -> (Node (g, t0, d, tr)),DSameH
+         | Lower -> dFitLeft g tl t0 d tr))
+   | CompGtT ->
+     (match delete x tr with
+      | DelNotFound -> DelNotFound
+      | Deleted dr ->
+        let t0,dc = dr in
+        Deleted
+        (match dc with
+         | DSameH -> (Node (g, tl, d, t0)),DSameH
+         | Lower -> dFitRight g tl d tr t0)))
+
---
+++ gaptree.mli
@@ -0,0 +1,125 @@
+
+
+type 'a option =
+| Some of 'a
+| None
+
+type comparison =
+| Eq
+| Lt
+| Gt
+
+type compareSpecT =
+| CompEqT
+| CompLtT
+| CompGtT
+
+type 'a sig0 =
+  'a
+  (* singleton inductive, whose constructor was exist *)
+
+type sumbool =
+| Left
+| Right
+
+type 'a eqDec = 'a -> 'a -> sumbool
+
+type 'a ordered = { eq_dec : 'a eqDec; compare : ('a -> 'a -> comparison); compare_spec : ('a -> 'a -> compareSpecT) }
+
+val compare_spec : 'a1 ordered -> 'a1 -> 'a1 -> compareSpecT
+
+type a (* AXIOM TO BE REALIZED *)
+
+val ordA : a ordered
+
+type gap =
+| G1
+| G0
+
+type gaptree =
+| Leaf
+| Node of gap * gaptree * a * gaptree
+
+type findResult =
+| Found
+| NotFound
+
+val find : a -> gaptree -> findResult
+
+val gof : gaptree -> gap option
+
+val setGap : gap -> gaptree -> gaptree
+
+val regapAs : gaptree -> gaptree -> gaptree
+
+type regapR =
+  gaptree
+  (* singleton inductive, whose constructor was regapR *)
+
+val regapAs' : gaptree -> gaptree -> regapR
+
+type gapnode =
+  gaptree
+  (* singleton inductive, whose constructor was Gapnode *)
+
+type ires =
+| ISameH
+| Higher
+
+type insertResult =
+| FoundByInsert
+| Inserted of gaptree * ires
+
+val rotateRight : gaptree -> a -> gaptree -> gap -> gapnode
+
+val rotateLeft : gaptree -> a -> gaptree -> gap -> gapnode
+
+val insert : a -> gaptree -> insertResult
+
+type dres =
+| DSameH
+| Lower
+
+type tryLowerResult =
+| Lowered of gaptree
+| LowerFailed
+
+val tryLowering : gaptree -> tryLowerResult
+
+val dRotateLeft : gaptree -> a -> gaptree -> gap -> gapnode
+
+type delminResult =
+| NoMin
+| MinDeleted of a * ( * )
+
+val dFitLeft : gap -> gaptree -> gaptree -> a -> gaptree -> ( * )
+
+val delmin : gaptree -> delminResult
+
+val dRotateRight : gaptree -> a -> gaptree -> gap -> gapnode
+
+val dFitRight : gap -> gaptree -> a -> gaptree -> gaptree -> ( * )
+
+type delmaxResult =
+| NoMax
+| MaxDeleted of a * ( * )
+
+val delmax : gaptree -> delmaxResult
+
+type deleteResult =
+| DelNotFound
+| Deleted of ( * )
+
+type twoGaps =
+| NoneNone
+| G0G0
+| G1G1
+| NoneG0
+| G1G0
+| G0None
+| G0G1
+
+val gof2 : gaptree -> gaptree -> twoGaps
+
+val delete : a -> gaptree -> deleteResult
+
